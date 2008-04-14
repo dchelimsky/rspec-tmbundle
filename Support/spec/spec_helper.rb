75bb140fadf22e077b802ae7db69f0f2427c65b2
David Chelimsky <dchelimsky@gmail.com>
2008-04-14 00:11:10 -0400

    Move relative location of rspec (core) directory.

--- Support/spec/spec_helper.rb
+++ Support/spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'stringio'
 
 module Spec::Example::ExampleMethods
   def set_env
-    root = File.expand_path(File.dirname(__FILE__) + '/../../../rspec')
+    root = File.expand_path(File.dirname(__FILE__) + '/../../../example_rails_app/vendor/plugins/rspec')
     ENV['TM_SPEC'] = "ruby -I\"#{root}/lib\" \"#{root}/bin/spec\""
     ENV['TM_RSPEC_HOME'] = "#{root}"
     ENV['TM_PROJECT_DIRECTORY'] = File.expand_path(File.dirname(__FILE__))
