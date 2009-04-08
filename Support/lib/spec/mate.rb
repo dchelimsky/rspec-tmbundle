59fdb28c2781830f06d56c0af59522631d446894
David Chelimsky <dchelimsky@gmail.com>
2009-04-08 04:10:25 -0500

    require 'spec/autorun'

--- Support/lib/spec/mate.rb
+++ Support/lib/spec/mate.rb
@@ -11,7 +11,7 @@ elsif ENV['TM_RSPEC_HOME']
   end
   $LOAD_PATH.unshift(rspec_lib)
 end
-require 'spec'
+require 'spec/autorun'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/..')
 require 'spec/mate/runner'
